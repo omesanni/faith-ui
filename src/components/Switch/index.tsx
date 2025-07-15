fe58bb5d81c210cacd276550814c16ff0b65443c
Ome Sanni <ome@Omes-MacBook-Pro.local>
2025-07-15 19:42:15 -0400

    Change to pixels

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -20,13 +20,13 @@ function Switch({
   onClick,
 }: Props) {
   const btnClasses = classNames(
-    'h-[1.125rem] w-8 shadow-xs rounded-full transition-colors duration-200 ease-in-out',
+    'h-[18px] w-8 shadow-xs rounded-full transition-colors duration-200 ease-in-out',
     { 'bg-[#dddddd]': !checked, 'bg-blue-600': checked },
     { 'disabled:pointer-events-none disabled:opacity-50': disabled },
     className,
   );
   const spanClasses = classNames(
-    'w-4 h-4 block ml-[0.0625rem] mr-[0.0625rem] transition duration-400 shadow-[0px_0px_4px_0px_rgba(0,0,0,0.2)] rounded-full bg-white',
+    'w-4 h-4 block ml-[1px] mr-[1px] transition duration-400 shadow-[0px_0px_4px_0px_rgba(0,0,0,0.2)] rounded-full bg-white',
     { 'translate-x-[calc(100%-2px)]': checked },
   );
 

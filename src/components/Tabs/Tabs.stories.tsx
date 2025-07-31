b02784ff4626a4978406ee722f9ff1bdc8747cbb
Ome Sanni <ome@Omes-MacBook-Pro.local>
2025-07-30 23:25:50 -0400

    Add custom classes story

--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -11,59 +11,74 @@ const meta: Meta<typeof Tabs> = {
 
 export default meta;
 
-const getTabs = (props: Omit<Props, 'children'> & Args): React.JSX.Element => (
+const getTabs = (
+  props: Omit<Props, 'children'> & Args,
+  disabledList?: Record<string, boolean>,
+  tabClassName?: string,
+): React.JSX.Element => (
   <Tabs {...props}>
     <Tab
       id="tab1"
       header="Tab 1"
+      className={tabClassName}
     >
       <div className="p-1">Tab 1</div>
     </Tab>
     <Tab
       id="tab2"
       header="Tab 2"
+      disabled={disabledList?.tab2}
+      className={tabClassName}
     >
       <div className="p-1">Tab 2</div>
     </Tab>
     <Tab
       id="tab3"
       header="Tab 3"
+      className={tabClassName}
     >
       <div className="p-1">Tab 3</div>
     </Tab>
     <Tab
       id="tab4"
       header="Tab 4"
+      className={tabClassName}
     >
       <div className="p-1">Tab 4</div>
     </Tab>
     <Tab
       id="tab5"
       header="Tab 5"
+      disabled={disabledList?.tab5}
+      className={tabClassName}
     >
       <div className="p-1">Tab 5</div>
     </Tab>
     <Tab
       id="tab6"
       header="Tab 6"
+      className={tabClassName}
     >
       <div className="p-1">Tab 6</div>
     </Tab>
     <Tab
       id="tab7"
       header="Tab 7"
+      className={tabClassName}
     >
       <div className="p-1">Tab 7</div>
     </Tab>
     <Tab
       id="tab8"
       header="Tab 8"
+      className={tabClassName}
     >
       <div className="p-1">Tab 8</div>
     </Tab>
     <Tab
       id="tab9"
       header="Tab 9"
+      className={tabClassName}
     >
       <div className="p-1">Tab 9</div>
     </Tab>
@@ -263,6 +278,109 @@ export const ScrollableOverflowButtons: StoryObj<typeof Tabs> = {
             orientation: 'vertical',
             mainClassName: 'mt-8 h-[280px]',
           })}
+        </section>
+      </div>
+    );
+  },
+};
+
+export const DisabledTabs: StoryObj<typeof Tabs> = {
+  parameters: {
+    controls: { include: ['orientation'] },
+  },
+  render: function Render(args: Args): React.JSX.Element {
+    const [primaryTab, setPrimaryTab] = useState('tab1');
+    const [secondaryTab, setSecondaryTab] = useState('tab1');
+
+    return (
+      <div>
+        <section>
+          <p className="mb-5 font-bold">
+            Primary Variant
+          </p>
+          {getTabs(
+            {
+              ...args,
+              variant: 'primary',
+              activeTab: primaryTab,
+              onChange: setPrimaryTab,
+            },
+            { tab2: true, tab5: true },
+          )}
+        </section>
+        <section className="mt-5">
+          <p className="mb-5 font-bold">
+            Secondary Variant
+          </p>
+          {getTabs(
+            {
+              ...args,
+              variant: 'secondary',
+              activeTab: secondaryTab,
+              onChange: setSecondaryTab,
+            },
+            { tab2: true, tab5: true },
+          )}
+        </section>
+      </div>
+    );
+  },
+};
+
+export const CustomClasses: StoryObj<typeof Tabs> = {
+  args: {
+    indicatorClassName: 'bg-orange-500',
+    backwardScrollButtonClassName: 'bg-sky-50',
+    forwardScrollButtonClassName: 'bg-sky-50',
+    tabListClassName: `
+      border-0 [&>button:not([aria-selected="true"]):hover]:text-orange-500
+      [&>button.primary-tab[aria-selected="true"]]:text-orange-500
+    `,
+  },
+  parameters: {
+    controls: {
+      include: [
+        'orientation',
+        'tabListClassName',
+        'indicatorClassName',
+        'forwardScrollButtonClassName',
+        'backwardScrollButtonClassName',
+      ],
+    },
+  },
+  render: function Render(args: Args): React.JSX.Element {
+    const [primaryTab, setPrimaryTab] = useState('tab1');
+    const [secondaryTab, setSecondaryTab] = useState('tab1');
+
+    return (
+      <div>
+        <section>
+          <p className="mb-5 font-bold">
+            Primary Variant
+          </p>
+          {getTabs(
+            {
+              ...args,
+              variant: 'primary',
+              activeTab: primaryTab,
+              onChange: setPrimaryTab,
+            },
+            undefined,
+            'primary-tab',
+          )}
+        </section>
+        <section className="mt-5">
+          <p className="mb-5 font-bold">
+            Secondary Variant
+          </p>
+          {getTabs(
+            {
+              ...args,
+              variant: 'secondary',
+              activeTab: secondaryTab,
+              onChange: setSecondaryTab,
+            },
+          )}
         </section>
       </div>
     );
